57f809cd6bc6e883aa1603504d31f73278716e68
jgbeasley <jgbeasley@users.noreply.github.com>
2018-04-10 11:23:53 -0700

    Create Mailing.cs

---
+++ Mailing.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstLook.Models
+{
+    public class Mailing
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+    }
+}
